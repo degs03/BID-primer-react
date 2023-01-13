3a355444c05f1a029f6cc68afa4835ff46ec778f
Dario Gimenez <gimenezdario1617@gmail.com>
2023-01-12 22:05:37 -0300

    mi primer react

---
+++ src/listcomponent.jsx
@@ -0,0 +1,18 @@
+import { Component } from "react";
+class listcomponent extends Component {
+    render() { 
+        return (
+            <>
+                <ul>
+                    <li>Learn React</li>
+                    <li>Climb Mt. Everest</li>
+                    <li>Run a marathon</li>
+                    <li>Feed the dogs</li>
+                </ul>
+
+            </>
+        );
+    }
+}
+
+export default listcomponent;
---
+++ src/titulocomponent.jsx
@@ -0,0 +1,12 @@
+import { Component } from "react";
+class Titulocomponent extends Component {
+    render() { 
+        return (
+            <>
+                <h1>Hello Dojo!</h1>
+                <h2>Things i need to do:</h2>
+            </>
+        );
+    }
+}
+export default Titulocomponent;
